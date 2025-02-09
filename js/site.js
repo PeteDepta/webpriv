9e805de92fcda3f1b7054ffefe1aa5c62dba8731
Meph384 <jakub.kozak384@gmail.com>
2025-02-09 19:11:29 +0100

    feat: contact us prep style: header fix: border animation after refresh refactor: missing px + other small fixes

--- js/site.js
+++ js/site.js
@@ -54,11 +54,6 @@ $(window).load(function () { // makes sure the whole site is loaded
 	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
 	$('body').delay(350).css({'overflow-y': 'visible'});
 
-	/* WOW Elements */
-	if (typeof WOW == 'function') {
-		new WOW().init();
-	}
-
 	/* Parallax Effects */
 	if (!!$.prototype.enllax) {
 		$(window).enllax();
